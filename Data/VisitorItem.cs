26df178d19abb50f0e1aeed74b4d91e9efbcc71e
mohammed mosaad mohammed <mohammed.mosaad@cis.asu.edu.eg>
2021-01-16 07:34:25 +0200

    update

---
+++ Data/VisitorItem.cs
@@ -0,0 +1,7 @@
+public class VisitorItem
+{
+    public string Name { get; set; }
+    public string Company { get; set; }
+    public bool IsAccepted { get; set; }
+    public bool IsRejected { get; set; }
+}
